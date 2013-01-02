029500792277dced2c80d6f84014ab767c9de657
L. Fernandez <luis@greenhost.nl>
2013-01-02 18:03:27 +0100

    Reworked the way Goteo gets translations for the admin panel

--- library/i18n/Lang.php
+++ library/i18n/Lang.php
@@ -19,11 +19,12 @@
  */
 
 
-namespace Goteo\Library {
+namespace Goteo\Library\i18n {
 
-  require_once 'library/php-mo/php-mo.php';  // external library to compile .po gettext files on the fly
+	require_once 'library/php-mo/php-mo.php';  // external library to compile .po gettext files on the fly
 
 	use Goteo\Core\Model;
+
 	/*
 	 * Clase para sacar textos estáticos de la tabla text
 	 *  (por ahora utilizar gettext no nos compensa, quizás más adelante)
@@ -127,88 +128,8 @@ namespace Goteo\Library {
 			$sql = "SELECT locale FROM lang WHERE id = :id";
 			$query = Model::query($sql, array(':id' => \LANG));
 			return $query->fetchColumn();
-    }
-
-		static protected function gettextBinaryExists($locale, $domain) {
-			return \file_exists("locale/{$locale}/LC_MESSAGES/{$domain}.mo");
-		}
-		
-		static protected function localeExists($locale, $domain) {
-			return \file_exists("locale/{$locale}/LC_MESSAGES/{$domain}.po");
-		}
-		
-		static public function gettextSupported() {
-			return function_exists("gettext");
 		}
 
-		/**
-		 * Use the php.mo library to compile gettext .po files if the binary doesn't exist.
-		 *
-		 * @return true/false on success/failure
-		 */
-		static private function compileLanguageFile($locale, $domain) {
-				return \phpmo_convert( "locale/{$locale}/LC_MESSAGES/{$domain}.po" );
-		}
-
-		/**
-		 * bypass gettext caching by using a clever file-renaming 
-		 * mechanism described in http://blog.ghost3k.net/articles/php/11/gettext-caching-in-php
-		 */
-		static protected function spawnUncachedDomain($locale, $domain) {
-			// path to the .MO file that we should monitor
-			$filename = "locale/{$locale}/LC_MESSAGES/{$domain}.mo";
-			$mtime = \filemtime($filename); // check its modification time
-			// our new unique .MO file
-			$filename_new = "locale/{$locale}/LC_MESSAGES/{$domain}_{$mtime}.mo"; 
-
-			if (!\file_exists($filename_new)) {  // check if we have created it before
-	      // if not, create it now, by copying the original
-	      \copy($filename,$filename_new);
-				//error_log("creating new domain {$filename_new}");
-			}
-			// compute the new domain name
-			$domain_new = "{$domain}_{$mtime}";
-			
-			return $domain_new;
-		} 
-		
-		/**
-		 * Set gettext configuration to be used by PHP.
-		 *
-		 * @param $locale the string that determines the current locale (e.g. en_GB)
-		 * @param $domain the filename for the .po file used by gettext to load messages from
-		 */
-		static public function gettext($locale, $domain) {
-			if( !Lang::gettextSupported() ) { 
-				error_log("ERROR - GETTEXT not supported on this server, all texts will appear in spanish"); 
-				return; 
-			}
-			
-			\setlocale(\LC_TIME, $locale);
-			\putenv("LANG={$locale}");
-			\setlocale(LC_ALL, $locale);
-			
-			if( Lang::localeExists($locale, $domain) ) {
-				// determine if the language binary file exists, if not try to generate it automatically
-				if( !Lang::gettextBinaryExists($locale, $domain) ) {
-					Lang::compileLanguageFile($locale, $domain);
-					//error_log("compiling missing language file binary");
-				}
-
-				// generate a new uncached domain file if caching bypass featured is enabled
-				if(true == \GOTEO_GETTEXT_BYPASS_CACHING) {	
-					$domain = Lang::spawnUncachedDomain($locale, $domain);
-					//error_log("bypassing gettext caching");
-				}
-			
-				// configure settext domain
-				\bindtextdomain($domain, "locale");
-				\bind_textdomain_codeset($domain, 'UTF-8');
-				\textdomain($domain);
-			} else {
-				error_log("WARNING - Locale is not installed ${locale}");
-			}
-		}
 	} // class
 
 
