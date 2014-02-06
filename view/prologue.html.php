1c38bda105838daf9056367442e29f26a84dc3ee
Julian Canaves <julian.canaves@gmail.com>
2014-02-06 23:06:07 +0100

    Facebook like button

--- view/prologue.html.php
+++ view/prologue.html.php
@@ -68,16 +68,22 @@
     </head>
 
     <body<?php if (isset($bodyClass)) echo ' class="' . htmlspecialchars($bodyClass) . '"' ?>>
-<?php if (isset($fbCode)) : ?>
+<?php /* 
+*
+*** Uncomment this lines and change __YOUR_APP_ID__
+*
+if (isset($fbCode)) : ?>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/<?php echo \Goteo\Library\i18n\Lang::locale(); ?>/all.js#xfbml=1&appId=189133314484241";
+  js.src = "//connect.facebook.net/<?php echo \Goteo\Library\i18n\Lang::locale(); ?>/all.js#xfbml=1&appId=__YOUR_APP_ID__";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<?php endif; ?>
+<?php endif;  
+*
+*/ ?>
         <script type="text/javascript">
             // Mark DOM as javascript-enabled
             jQuery(document).ready(function ($) {
